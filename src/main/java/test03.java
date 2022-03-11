d6e88d4f5f856c17bfab8c23e56394fe2e293436
Bekzat013 <101384968+Bekzat013@users.noreply.github.com>
2022-03-11 12:28:48 +0600

    Add files via upload

---
+++ src/main/java/test03.java
@@ -0,0 +1,15 @@
+public class test03 {
+
+    public int getSum(int x, int y) {
+        return x+y;
+    }
+
+    public int getDivide(int x, int y) {
+        return x/y;
+    }
+
+    public int getMultiple(int x, int y) {
+        return x*y;
+    }
+
+}
---
+++ src/test/java/test03Test.java
@@ -0,0 +1,60 @@
+import org.junit.*;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by retinka on 13.01.2016.
+ */
+public class test03Test {
+
+    private test03 calculator;
+
+    @BeforeClass
+    public static void beforeClass() {
+        System.out.println("Before CalculatorTest.class");
+    }
+
+    @AfterClass
+    public  static void afterClass() {
+        System.out.println("After CalculatorTest.class");
+    }
+
+    @Before
+    public void initTest() {
+        calculator = new test03();
+    }
+
+    @After
+    public void afterTest() {
+        calculator = null;
+    }
+
+    @Test
+    public void testGetSum() throws Exception {
+        assertEquals(11, calculator.getSum(7,8));
+    }
+
+    @Test
+    public void testGetDivide() throws Exception {
+        assertEquals(20, calculator.getDivide(100,5));
+    }
+
+    @Test
+    public void testGetMultiple() throws Exception {
+
+    }
+
+    @Test(expected = ArithmeticException.class)
+    public void divisionWithException() {
+        calculator.getDivide(15,0);
+    }
+
+    @Ignore("Message for ignored test")
+    @Test
+    public void ignoredTest() {
+        System.out.println("will not print it");
+    }
+
+
+
+}
